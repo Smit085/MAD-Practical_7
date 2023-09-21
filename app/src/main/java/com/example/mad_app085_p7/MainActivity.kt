d6b74b88232ef90e7dcc5a2832ea5eb3d444dd0b
Smit <2001smitpatel@gmail.com~>
2023-09-21 12:36:00 +0530

    i

--- app/src/main/java/com/example/mad_app085_p7/MainActivity.kt
+++ app/src/main/java/com/example/mad_app085_p7/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.mad_app085_p7
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate (savedInstancestate: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstancestate)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initiatePlayer()
+        binding.btn_switch
     }
 }
