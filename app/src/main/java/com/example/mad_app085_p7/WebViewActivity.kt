824f0c3a1fb96be027ce46e8145afbb1a524b5f8
Smit <2001smitpatel@gmail.com~>
2023-09-23 19:36:58 +0530

    f

--- app/src/main/java/com/example/mad_app085_p7/WebViewActivity.kt
+++ app/src/main/java/com/example/mad_app085_p7/WebViewActivity.kt
@@ -1,11 +1,9 @@
 package com.example.mad_app085_p7
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebSettings
-import android.widget.MediaController
 import androidx.core.view.WindowCompat
 import com.example.mad_app085_p7.databinding.ActivityWebViewBinding
 
@@ -22,7 +20,14 @@ class WebViewActivity : AppCompatActivity() {
             Intent(this,MainActivity::class.java).also{ startActivity(it) }
         }
     }
+    private val youtubeld = "6stlCkUDG_s"
+
     private fun initWebVideoPlayer(){
-        val webSettings: WebSettings = binding.webvideoView.Settings
+        val webSettings: WebSettings = binding.webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.loadWithOverviewMode = true
+        webSettings.useWideViewPort = true
+        binding.webView.loadUrl("https://www.youtube.com/embed/$youtubeld")
+
     }
 }
